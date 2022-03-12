45276914e4addb1de280dd3cbf053f90ba9bbe98
Cameron Moore <cameronmoore@Camerons-MacBook-Air.local>
2022-03-12 14:03:57 -0500

    Folder restructure

--- src/main/java/com/example/controller/GreetingController.java
+++ src/main/java/com/example/controller/GreetingController.java
@@ -1,7 +1,8 @@
-package com.example.restservice;
+package com.example.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.example.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

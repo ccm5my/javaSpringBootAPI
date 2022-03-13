df7847f75a92ad8f1e7b45eb118279134bd9b643
Cameron Moore <cameronmoore@Camerons-MacBook-Air.local>
2022-03-13 13:32:50 -0400

    Got the connect method working

--- src/main/java/com/example/controller/BaseController.java
+++ src/main/java/com/example/controller/BaseController.java
@@ -1,18 +1,21 @@
 package com.example.controller;
 
+import com.example.interfaces.ISQLite;
+import com.example.service.SQLite;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
 
+	private ISQLite sql;
 	public BaseController() {
-
+		sql = new SQLite();
 	}
 
 	@GetMapping("/connect")
 	public void connect() {
-
+		sql.connect();
 		System.out.println("Tested database connection");
 	}
 }
--- src/main/java/com/example/service/SQLite.java
+++ src/main/java/com/example/service/SQLite.java
@@ -2,10 +2,26 @@ package com.example.service;
 
 import com.example.interfaces.ISQLite;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class SQLite implements ISQLite {
 
     @Override
     public void connect() {
+        Connection conn = null;
+        Statement stmt = null;
 
+        try {
+            String url = "jdbc:sqlite:movies.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ACTORS LIMIT 1;");
+            rs.close();
+        } catch(Exception e) {
+        }
     }
 }
